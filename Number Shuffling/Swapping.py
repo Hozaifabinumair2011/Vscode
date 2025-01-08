12b05f52524829a3c102b4ee6a0d86ceee33217e
Hozaifabinumair2011 <umairarshad85@gmail.com>
2025-01-09 02:05:33 +0500

    Swapping Numbers

---
+++ Number Shuffling/Swapping.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number "))
+b=int(input("Enter a number "))
+c=int(input("Enter a number "))
+print("The numbers before swapping",(a,b,c))
+e=a
+a=c
+b=b
+c=e
+print("The numbers after swapping",(a,b,c))
+# Swapping of three numbers.
