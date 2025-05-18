9de731212550736a144ffd315d4088bbf8958196
Hozaifabinumair2011 <umairarshad85@gmail.com>
2025-05-18 14:49:26 +0500

    Denomination Calculator

--- Denomination calculator/main.py
+++ Denomination calculator/main.py
@@ -49,20 +49,20 @@ def topwin():
         except ValueError:
             messagebox.showerror("Error", "Please enter a valid number.")
 
-            btn= Button(top, text='Calculate', command=calculator, bg='brown', fg='white')
+    btn= Button(top, text='Calculate', command=calculator, bg='brown', fg='white')
 
-            label.place(x=230, y=50   )
-            entry.place(x=200, y=80   )
-            btn.place(x=240, y=120   )
-            lbl.place(x=140, y=170   )
+    label.place(x=230, y=50)
+    entry.place(x=200, y=80)
+    btn.place(x=240, y=120)
+    lbl.place(x=140, y=170)
 
-            l1.place(x=180, y=200   )
-            l2.place(x=180, y=230   )
-            l3.place(x=180, y=260   )
+    l1.place(x=180, y=200)
+    l2.place(x=180, y=230)
+    l3.place(x=180, y=260)
 
-            t1.place(x=270, y=200   )
-            t2.place(x=270, y=230   )
-            t3.place(x=270, y=260)
+    t1.place(x=270, y=200)
+    t2.place(x=270, y=230)
+    t3.place(x=270, y=260)
 
     top.mainloop()
 root.mainloop()
